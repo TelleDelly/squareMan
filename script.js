c1293e19d809d9e53f7e946c698419860e644ed3
tellydelly <martinwilson450@gmail.com>
2022-03-05 12:48:29 -0800

    whiteboarding and psuedo code keyframes for projectiles

--- script.js
+++ script.js
@@ -9,17 +9,74 @@
 
 // document.addEventListener('mousemove', onMouseMove)
 
-const circle = document.body.querySelector('.circle');
-const onMouseMove = (e) =>{
-    // console.log('moving')
-  circle.style.left = e.pageX + 'px';
-  circle.style.top = e.pageY + 'px';
-}
+//whiteboarding
+/*
+Map: The entire viewport is the map the player can move around by using the mouse
 
-document.addEventListener('mousemove', onMouseMove);
+Player:
+Will have a square body which can be moved around using the mouse
+W-key will shoot a projectile up from the player
+A-key will shoot a projectile to the left of the player
+S-key will shoot a projectile down from the player
+D-key will shoot a projectile to the right of the player
 
-circle.addEventListener('click', (e) => {
-    const projectile = document.createElement('div')
-    projectile.className = 'projectile'
-    circle.appendChild(projectile)
+Each projectile will have the same speed and will travel until it is off screen(use vh and vw)
+Once a projectile is off screen(once the animation is done it will be removed as a child from the parent object(see animationEnd event callbacks)
+
+If a player is hit or runs into a target the player will lose the game and a game over modal will appear
+
+Target:
+Targets will fly in off the screen at "random" determined from an array of possible points and paths using random selection
+Target will have the same speed and will no accelerate during their flight
+There will only be three targets on the screen at any time
+Once a target is off screen it will be removed from the page
+If a target is hit by a projectile it will be destroyed and points will be added to the players tally
+If a target is not hit by a projectile no points will be added and the target will be removed from the game
+
+Game:
+The game objective is to get a highscore or play untill death
+The player score will be located in the top right of the screen
+It will start at zero and increment for each target destroyed
+*/
+
+//psuedocode
+/*
+playerObject.style.left will be equal to the cursor x of a mouse move event
+playerObject.style.top will be equal to the cursor y of a mouse move event
+the player object will have a cursor.style of none
+the playerObject height and width will be equal
+
+projectiles will be created from the center of the playerObject and will follow an animation transform
+let projectile = Document.createElement('div')
+projectile.classList.add('projectile WHAT_TYPE_OF_SHOT')
+projectile.addEventListener('animationend',()=>{
+  playerObject.removeChild(projectile)
 })
+playerObject.appendChild(projectile)
+
+when projectile animationend ===true the object will be deleted from the dom
+
+an eventlistener on the window body will check to see if animationstart has occured
+window.addEventListener('animationstart' (e)=>{
+  if(target){
+    do{
+      if(target intersects player){
+        gameover
+      }
+    } while(aniamtionend != true)
+  }
+
+  if(projectile){
+    if(projectile intersects target){
+      target is destroyed
+    }
+  } while(animationend != true)
+
+})
+
+window.addEventListener('animationend' (e) => {
+
+})
+
+*/
+
